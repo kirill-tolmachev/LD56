faad700667a60c6e86a9e425b001cb8e5f38eaef
Kirill Tolmachev <tolmachev.kirill.a@gmail.com>
2024-10-07 01:37:32 +0200

    Balance fix

--- Assets/Game/Scripts/States/Levels/Level4.cs
+++ Assets/Game/Scripts/States/Levels/Level4.cs
@@ -40,7 +40,7 @@ namespace Game.Scripts.States.Levels
             await _narrationUI.ShowText("However, we've noticed some unusual activity in the <b>SQUARES</b> operations team.", _narrators.Triangle);
             await _narrationUI.ShowText("It seems some <b>SQUARES</b> aren't behaving as expected.", _narrators.Triangle);
             await _narrationUI.ShowText("They've turned... <color=red>RED</color>.", _narrators.Triangle);
-            await _narrationUI.ShowText("While we figure out what's going on, please ensure <color=red><b>NO MORE THAN 3 RED SQUARES</b></color> get into the can.", _narrators.Triangle);
+            await _narrationUI.ShowText("While we figure out what's going on, please ensure <color=red><b>NO MORE THAN 5 RED SQUARES</b></color> get into the can.", _narrators.Triangle);
             await _narrationUI.ShowText("And remember, the previous requirements still apply: <b>SQUARES</b> on the left and <b>CIRCLES</b> on the right.", _narrators.Triangle);
 
             await _narrationUI.ShowText("Good luck!", _narrators.Triangle);
@@ -54,7 +54,7 @@ namespace Game.Scripts.States.Levels
             _levelState.InvalidNormalChance = 0.01f;
             
             var waitForWin = UniTask.WaitUntil(() => _levelState.CorrectWoos == 50, cancellationToken: cancellationToken);
-            var waitForLose = UniTask.WaitUntil(() => _levelState.WrongWoos == 10 || _levelState.TotalRedSquares >= 3, cancellationToken: cancellationToken);
+            var waitForLose = UniTask.WaitUntil(() => _levelState.WrongWoos == 10 || _levelState.TotalRedSquares > 5, cancellationToken: cancellationToken);
 
             int result = await UniTask.WhenAny(waitForWin, waitForLose);
 
